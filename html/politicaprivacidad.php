6f88cda9d09f1aa430def48a0403bf799418ea2d
Alexiiius <alexius1996@gmail.com>
2023-05-28 15:58:27 +0200

    Add files via upload

--- html/politicaprivacidad.php
+++ html/politicaprivacidad.php
@@ -10,7 +10,7 @@ require_once ("../php/conexion.php");
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cookies - Viñedos Edroiz</title>
+    <title>Politica Privacidad - Viñedos Edroiz</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="/css/estilo.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
