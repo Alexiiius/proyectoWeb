2367f5a1bf9ecb74bc429d87f5694a89faae16e0
Alexiiius <alexius1996@gmail.com>
2023-05-29 14:31:37 +0200

    Delete php directory

--- php/error.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    if (isset($_SESSION['errorMensaje'])) {
-        $errorMensaje = $_SESSION['errorMensaje'];
-        //echo "<script>alert('$errorMensaje');</script>";
-        echo " <script>  window.onload = function() {   alert('$errorMensaje'); };</script>";
-        unset($_SESSION['errorMensaje']);
-    }
-    
-?>
--- php/publicar.php
+++
@@ -1,8 +0,0 @@
-<h3 class="centrado">Publicar artículo</h3>
-<div class="centrado container-fluid"> <!-- div central-->
-                        <form method="post" action="">
-                            <input type="text" name="title" placeholder="Título del artículo" required><br>
-                            <textarea name="content" placeholder="Contenido del artículo" required></textarea><br>
-                            <input type="submit" name="publish" value="Publicar artículo">
-                        </form>    
-</div>
