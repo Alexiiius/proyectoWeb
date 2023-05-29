f2aec633ebcc7db7ee8de5bb1f0085edf1911b60
Alexiiius <alexius1996@gmail.com>
2023-05-29 14:31:27 +0200

    Delete sql directory

--- sql/MisVinosDB.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS MisVinosDB;
-USE MisVinosDB;
-
-create user if not exists usuario identified by 'usuario';
-GRANT ALL PRIVILEGES ON misvinosdb.* TO 'usuario'@"%";
-
-select * from usuarios;
-select * from articulos;
-select * from ratings;
